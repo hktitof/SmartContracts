8ff9f18340728dae7bf83039dedc0b0909dd24c7
titof <hktitof@gmail.com>
2022-08-17 10:53:28 +0100

    add verificaiton api and plugin for harhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,10 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
 dotenv.config();
-
+import "@nomiclabs/hardhat-ethers"
 const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 const RPC_URL = process.env.RPC_URL!;
+const ETHER_SCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
 const config: HardhatUserConfig = {
   // ? if you don't specify defaultNetwork it's by the default "hardhat" => defaultNetwork: "hardhat"
   solidity: "0.8.8",
@@ -16,6 +17,9 @@ const config: HardhatUserConfig = {
       chainId: 4,
     },
   },
+  etherscan:{
+    apiKey: ETHER_SCAN_API_KEY
+  }
 };
 
 export default config;
