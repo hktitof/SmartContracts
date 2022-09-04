fa5ed24c2c4cf52857c76cd56b497de1d93157de
titof <hktitof@gmail.com>
2022-09-04 11:57:22 +0100

    added solhint to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,30 @@
 // imports
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-waffle";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-ethers";
 import dotenv from "dotenv";
 dotenv.config();
-import "./tasks/block_number"
+import "./tasks/block_number";
 // gas reporter it tells us when we do our tests how much gas it cost either deploy or calling functions
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
 // coverage is used to know how our tests are
-import "solidity-coverage"
+import "solidity-coverage";
 // this one is used to give our Contract a correct typing
 // we use "yarn hardhat typechain" => create typechain-types folder with types for all our contracts
-import "@typechain/hardhat"
+import "@typechain/hardhat";
+
+// solhint : The linting solutions available for detecting code vulnerabilities of the Solidity programming 
+// language — underlying smart contracts in Ethereum — mostly focus on the code compliance with the best
+// linting practices.
+// usage : yrn hardhat check
+import "@nomiclabs/hardhat-solhint";
 
 // Variables
 const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 const RPC_URL = process.env.RPC_URL!;
 const ETHER_SCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
-const COINMARKETCAP_API_KEY=process.env.COINMARKETCAP_API_KEY!;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY!;
 
 const config: HardhatUserConfig = {
   // ? if you don't specify defaultNetwork it's by the default "hardhat" => defaultNetwork: "hardhat"
@@ -30,25 +36,25 @@ const config: HardhatUserConfig = {
       accounts: [PRIVATE_KEY!],
       chainId: 4,
     },
-    localhost:{
-      url : "http://127.0.0.1:8545/",
+    localhost: {
+      url: "http://127.0.0.1:8545/",
       //acctounts : they are automatically provided by hardhat, 19 fake accounts
-      chainId: 31337
-    }
+      chainId: 31337,
+    },
   },
-  etherscan:{
-    apiKey: ETHER_SCAN_API_KEY
+  etherscan: {
+    apiKey: ETHER_SCAN_API_KEY,
   },
-  gasReporter:{
+  gasReporter: {
     // change it to false to disable gas reporter
     enabled: false,
-    outputFile:"gas-report.txt",
-    noColors:true,
-    currency:"USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
     coinmarketcap: COINMARKETCAP_API_KEY,
     // specify the gas price "token" convert to USD, by the default it's ETH
     // token:"ETH"
-  }
+  },
 };
 
 export default config;
